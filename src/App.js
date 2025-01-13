d15ebd1736185bc150d622041236405bd134c487
Prashant Lambha <125307000+Prashant825@users.noreply.github.com>
2025-01-13 23:10:17 +0530

    new branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Weather from './templates/Weather';
 function App() {
   return (
     <div className="App">
-      <h2>testing..</h2>
       < Weather />
     </div>
   );
--- src/templates/Weather.js
+++ src/templates/Weather.js
@@ -1,31 +1,47 @@
 import axios from 'axios';
-import React, { useState} from 'react';
+import React, { useState } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { FaCloudSun, FaSun, FaCloudRain, FaSmog, FaCloud } from 'react-icons/fa';
+import AirIcon from '@mui/icons-material/Air';
 
 export default function Weather() {
 
   const [formData, setFormData] = useState('');
-  const [tempData, setTemp] =useState('');
-   const data = formData;
+  const [weatherData, setWeatherdata] = useState('');
+  const data = formData;
 
+  const {
 
-    const fatchWeather = async ()=>{
+    name,
+    main,
+    weather,
+    wind,
+    visibility,
+  } = weatherData || {};
 
-      try{
-        const response =  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=2cc711b1a7ffa12a235ee3b46f4e21c5&units=metric`)
-        
-        if(response){
-          const temp = response.data.main.temp;
-          console.log(temp);
-          setTemp(temp);
-          
-        }
-      }
-      catch(error){
-        console.log(error);
+  const { temp } = main || {};
+  const { description } = weather ? weather[0] : {};
+  const { speed } = wind || {};
+
+  const fatchWeather = async () => {
+
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=2cc711b1a7ffa12a235ee3b46f4e21c5&units=metric`)
+
+      if (response) {
+        const weatherData = response.data;
+        console.log(response.data);
+        setWeatherdata(weatherData);
+
       }
     }
+    catch (error) {
+      console.log(error);
+    }
+  }
 
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log('formData', formData);
     fatchWeather();
@@ -33,13 +49,42 @@ export default function Weather() {
   }
   return (
     <div>
-      <div className='waether-show-div'>
-       <form onSubmit={handleSubmit}>
-        <input type="text" name='name' value={formData} onChange={(e)=>setFormData(e.target.value)}/>
-        <button type="submit">Submit</button>
-       </form>
-       <p>{tempData ? `Temp: ${tempData}°C` : 'No temperature data available'}</p>      </div>
-      
+       <div>
+        <h2 className='weather-heading'>Check Weather Application</h2>
+        <div className='waether-show-div'>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name='name' value={formData} onChange={(e) => setFormData(e.target.value)} required />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
+        {/* ==========Weather Template========== */}
+
+        {weatherData ?
+        <div className="main">
+          <div className="details">
+            {name ? <h3>City: {name}</h3> : ''}
+            {temp ? <strong>Temperature: {temp}°C</strong> : ''}
+            <div className="forecast">
+              <div className="forecast-item">
+                <p>Cloud</p>
+                {description ? <div> {description === 'clear sky' ? <FontAwesomeIcon icon={faSun} size="2x" color="#FDB813" />: description === 'fog'? <FaSmog size={50} color="#808080" />: <FaCloudSun size={50} color="#FDB813" /> } 
+                  <p className="temp">{description}</p></div> : ''}
+              </div>
+              <div className="forecast-item">
+                <p>Visibility</p>
+                {visibility ? <div>{visibility > 8000?<FaSun size={50} color="#FDB813" />: visibility > 3000?<FaCloud size={50} color="#B0C4DE" />: visibility > 1000?<FaCloudRain size={50} color="#4682B4" />:<FaSmog size={50} color="#808080" />}
+                  <p className="temp">{visibility} km</p></div> : ''}
+              </div>
+              <div className="forecast-item">
+                <p>Wind</p>
+                {speed ? <div> <AirIcon fontSize="large" color="primary" />
+                  <p className="temp">Speed {speed} /H</p></div> : ''}
+              </div>
+            </div>
+          </div>
+        </div> : <p className='before-cheack-weather-desc'>Please Enter the city name in above input for check the weather.</p>}
+      </div> 
     </div>
   )
 }
